998f244580637dbc1c9752f6e0f1a2ed8326819d
WarrenWu4 <warrenweiwu04@gmail.com>
2024-03-25 13:43:31 -0500

    finished setting up basic frontend structure and included the necessary routing for this sprint

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Login from './pages/Login'
+import Menu from './pages/Menu'
+import Order from './pages/Order'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<PlaceHolder/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/menu" element={<Menu/>}/>
+                <Route path="/order" element={<Order/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
 )
+
+function PlaceHolder() {
+    return (
+        <>
+        </>
+    )
+}
