2f602bf9ae7c481b00204ceeb3f8c65f714f34ac
Beketovian <andrewbeketov03@gmail.com>
2024-03-31 18:37:44 -0500

    update navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,16 +1,27 @@
 export default function Navbar() {
+    function changeLang() {
+        console.log("language change")
+        // 
+    }
+
     return (
-        <nav className="text-black p-2 flex justify-between items-center shadow-md">
-            <div className="flex items-center">
-                <div className="square-black"><img src="./logo.png"></img></div>
-                <span className="font-bold text-lg ml-2 font-ptserif">Welcome to Rev's Grill</span>
+        <nav className="text-black pb-2 flex justify-between items-center shadow-md">
+            <div className="flex items-center flex-1">
+                <div className="square-black border-black border-[2px] rounded border"><img src="./logo.png" alt="Logo"></img></div>
+                <span className="font-bold text-4xl ml-2 font-ptserif">Welcome to Rev's Grill</span>
             </div>
-            <div className="flex items-center">
-                <button className="bg-black hover:bg-gray-600 text-white px-2 py-1 ml-2 rounded-md text-sm"><a href="/menu" className="text-gray-300 hover:text-wh px-3 py-1 rounded-md text-sm font-medium">view menu</a></button>
-                <button className="bg-black hover:bg-gray-600 text-white px-2 py-1 ml-2 rounded-md text-sm"><a href="/order" className="text-gray-300 hover:text-white px-3 py-1 rounded-md text-sm font-medium">start order</a></button>
-                <button className="bg-black hover:bg-gray-600 text-white px-2 py-1 ml-2 rounded-md text-sm"><a href="/login" className="text-gray-300 hover:text-white px-3 py-1 rounded-md text-sm font-medium">login</a></button>
-                <button className="bg-black hover:bg-gray-600 text-white px-2 py-1 ml-2 rounded-md text-sm">EN</button>
-                <button className="bg-black hover:bg-gray-600 text-white px-2 py-1 ml-2 rounded-md text-sm">+</button>
+            <div className="flex items-center justify-end flex-2">
+                <button className="border-[1px] border-black bg-white hover:bg-black hover:text-white px-4 py-2 ml-2 rounded-md text-lg font-medium font-ptserif">
+                    <a href="/menu" className="hover:text-white">view menu</a>
+                </button>
+                <button className="border-[1px] border-black bg-white hover:bg-black hover:text-white px-4 py-2 ml-2 rounded-md text-lg font-medium font-ptserif">
+                    <a href="/order" className="hover:text-white">start order</a>
+                </button>
+                <button className="border-[1px] border-black bg-white hover:bg-black hover:text-white px-4 py-2 ml-2 rounded-md text-lg font-medium font-ptserif">
+                    <a href="/login" className="hover:text-white">login</a>
+                </button>
+                <button onClick={changeLang} className="border-[1px] border-black bg-white hover:bg-black hover:text-white px-4 py-2 ml-2 rounded-md text-lg font-medium font-ptserif">EN</button>
+                <button className="border-[1px] border-black bg-white hover:bg-black hover:text-white px-4 py-2 ml-2 rounded-md text-lg font-medium font-ptserif">+</button>
             </div>
         </nav>
     )
