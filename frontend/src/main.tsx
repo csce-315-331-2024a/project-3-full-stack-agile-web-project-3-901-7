dd32dac9db4f86ad47e6cf46418e54ac6801e6c5
WarrenWu4 <warrenweiwu04@gmail.com>
2024-04-10 08:38:28 -0500

    added admin order page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,7 @@ import Login from './pages/Login'
 import Menu from './pages/Menu'
 import Order from './pages/Order/Order'
 import Manager from './pages/Manager'
+import AdminOrder from './pages/AdminOrder'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <GoogleOAuthProvider clientId="12221267435-lsk9h3j605atjq4n35dvpsf2gun7dh6a.apps.googleusercontent.com">
@@ -20,6 +21,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                     <Route path="/menu" element={<Menu/>}/>
                     <Route path="/order" element={<Order/>}/>
                     <Route path="/manager" element={<Manager/>}/>
+                    <Route path="/admin/order" element={<AdminOrder/>}/>
                 </Routes>
             </BrowserRouter>
         </React.StrictMode>
