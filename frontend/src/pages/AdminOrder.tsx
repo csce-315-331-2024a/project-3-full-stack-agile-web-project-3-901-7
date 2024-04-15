b78cb788b200ba68c1a2a70f0140f2e7a94f20f0
WarrenWu4 <warrenweiwu04@gmail.com>
2024-04-14 21:20:33 -0500

    added admin order page

--- frontend/src/pages/AdminOrder.tsx
+++ frontend/src/pages/AdminOrder.tsx
@@ -1,11 +1,12 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
-import { FaSearch } from "react-icons/fa";
-import { useState, useEffect } from "react";
 import { Item, OrderType } from "../types/dbTypes";
+import { FaMinus, FaPlus, FaSearch } from "react-icons/fa";
 
 export default function AdminOrder() {
 
     const categories = ["Burger", "Chicken", "Side", "Salad", "Snack", "Beverage", "Dessert", "Seasonal"]
+
     const [items, setItems] = useState<Item[]>([]);
     const [order, setOrder] = useState<OrderType>({
         numItems: 0,
@@ -14,48 +15,53 @@ export default function AdminOrder() {
         total: 0,
         date: new Date()
     });
-    const [currCategory, setCurrCategory] = useState<string>("Burger");
 
-useEffect(() => {
+    useEffect(() => {
 
         async function fetchItems() {
-        const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/item/findAll");
-        const data = await response.json();
-        setItems(data);
+            const response = await fetch(import.meta.env.VITE_BACKEND_URL + "/item/findAll");
+            const data = await response.json();
+            setItems(data);
         }
 
         fetchItems();
 
-        }, [])
+    }, [])
 
     return (
         <div className="w-full h-full p-8 flex flex-col gap-y-8">
 
             <Navbar/>
 
-            <div className="flex items-end gap-x-12">
-                <div className="border-b-2 border-black flex items-center gap-x-2 px-4 py-2 text-lg">
+            <div className="mt-8 flex items-end gap-x-12">
+                <div className="min-w-[320px] flex items-center border-b-2 border-black gap-x-4 px-4 py-2">
                     <FaSearch/>
-                    <input 
-                        type="text" 
+                    <input
+                        type="text"
                         placeholder="search item"
-                        className="px-2 py-1 font-ptserif placeholder:text-black/60" 
+                        className="w-full px-2 py-1 font-ptserif text-xl"
                     />
                 </div>
-            
                 <button
                     type="button"
-                    onClick={() => console.log("btn")}
-                    className={"bg-black rounded-md px-4 py-3 font-ptserif font-bold text-white text-base"}
+                    onClick={() => console.log("order submitted")}
+                    className="px-4 py-3 bg-black rounded-md font-ptserif font-lg text-white"
                 >
                     submit order
                 </button>
             </div>
 
-            <div className="flex flex-wrap gap-16">
-                {categories.map((category:string) => {
-                    const categoryItems:Item[] = items.filter((item) => item.category === category)
-                    return <AdminOrderBlock title={category} items={categoryItems}/>
+            <div className="mt-8 flex flex-wrap gap-16 pb-12">
+                {categories.map((category, index) => {
+                    return (
+                        <AdminOrderItemContainer
+                            key={index}
+                            title={category}
+                            items={items.filter((item:Item) => item.category === category)}
+                            order={order}
+                            setOrder={setOrder}
+                        />
+                    )
                 })}
             </div>
 
@@ -63,28 +69,71 @@ useEffect(() => {
     )
 }
 
-interface AdminOrderBlockProps {
+interface AdminOrderItemContainerProps {
     title: string;
-    items: Item[]; 
+    items: Item[];
+    order: OrderType;
+    setOrder: React.Dispatch<React.SetStateAction<OrderType>>;
 }
 
-function AdminOrderBlock({title, items} : AdminOrderBlockProps) {
+function AdminOrderItemContainer({title, items, order, setOrder}: AdminOrderItemContainerProps) {
+
+    function increaseItem(price:number, name: string, id:number) {
+        setOrder({
+            ...order,
+            numItems: order.numItems + 1,
+            total: order.total + price,
+            orderInfo: order.orderInfo + name + " ",
+            itemToQuantity: order.itemToQuantity.set(id, (order.itemToQuantity.has(id)) ? order.itemToQuantity.get(id)! + 1 : 1)
+        })
+    }
+
+    function decreaseItem(price:number, name:string, id:number) {
+        if (!order.itemToQuantity.has(id))
+            return
+        if (order.itemToQuantity.get(id)! <= 0)
+            return
+        setOrder({
+            ...order,
+            numItems: order.numItems - 1,
+            total: order.total - price,
+            orderInfo: order.orderInfo.replace(name + " ", ""),
+            itemToQuantity: order.itemToQuantity.set(id, order.itemToQuantity.get(id)! - 1)
+        })
+    }
 
     return (
-        <div>
-            <h4 className="font-ptserif font-bold text-xl mb-2">{title}</h4>
-            {items.map((item:Item) => {
-                return (
-                    <div className="flex gap-x-2 mb-1">
-                        <p>{item.name}</p>
-                        <button>0</button>
-                        <p>{item.price}</p>
-                    </div>
-            )})}
+        <div className="flex flex-col gap-y-2">
+            <h4 className="font-bold text-2xl font-ptserif">{title}</h4>
+            <div>
+                {items.map((item:Item) => {
+                    return (
+                        <div key={item._id} className="flex items-center gap-x-6 font-ptserif text-lg">
+                            <p>{item.name}</p>
+                            <div className="flex gap-x-2 items-center font-bold">
+                                <button
+                                    type="button"
+                                    onClick={() => increaseItem(item.price, item.name, item._id)}
+                                    className="w-5 h-5 border-2 border-black rounded-full flex items-center justify-center p-1"
+                                >
+                                    <FaPlus/>
+                                </button>
+                                <p>
+                                    {(order.itemToQuantity.has(item._id)) ? order.itemToQuantity.get(item._id) : 0}
+                                </p>
+                                <button
+                                    type="button"
+                                    onClick={() => decreaseItem(item.price, item.name, item._id)}
+                                    className="w-5 h-5 border-2 border-black rounded-full flex items-center justify-center p-1"
+                                >
+                                    <FaMinus/>
+                                </button>
+                            </div>
+                            <p className="font-bold">${item.price}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
-
-
-
-
