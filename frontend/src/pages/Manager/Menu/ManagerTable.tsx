65993ce466a1e148d01597f42ef882782a33629f
Surya Jasper <45545431+suryajasper@users.noreply.github.com>
2024-04-26 22:19:59 -0500

    Added sorting to manager table

--- frontend/src/pages/Manager/Menu/ManagerTable.tsx
+++ frontend/src/pages/Manager/Menu/ManagerTable.tsx
@@ -9,11 +9,52 @@ interface IManagerTableProps {
   data: any[][];
 }
 
+type SortOrdering = 'ascending' | 'descending'
+
+interface ISortOptions {
+  column: number;
+  order: SortOrdering;
+}
+
+function compare(a : any, b : any, order : SortOrdering) {
+  let orderFactor = ((order === 'ascending') ? 1 : -1);
+
+  if (!b)
+    return orderFactor;
+  if (!a)
+    return -orderFactor;
+
+  if (typeof a === 'string' && typeof b === 'string')
+    return a.localeCompare(b) * orderFactor;
+
+  if (typeof a === 'number' && typeof b === 'number')
+    return (a - b) * orderFactor;
+
+  if (typeof a === 'object' && typeof b === 'object')
+    return (a > b) ? orderFactor : -orderFactor;
+
+  return 0;
+}
+
 const ManagerTable : React.FC<IManagerTableProps> = (props) => {
   const [hoveredRowIndex, setHoveredRowIndex] = useState<number | null>(null);
 
   const handleRowHover = (index: number | null) => {
     setHoveredRowIndex(index);
+  };
+
+  const [sortOptions, setSortOptions] = useState<ISortOptions>({
+    column: 0,
+    order: 'ascending',
+  });
+
+  const toggleSort = (column : number) => {
+    setSortOptions((prevSort) => {
+      let newSort : ISortOptions = { column, order: 'ascending' };  
+      if (prevSort.column === column)
+        newSort.order = prevSort.order === 'ascending' ? 'descending' : 'ascending';
+      return newSort;
+    });
   };
 
   const [userProfile, setUserProfile] = useState<User | undefined>(undefined);
@@ -32,66 +73,72 @@ const ManagerTable : React.FC<IManagerTableProps> = (props) => {
             {props.thumbnails && (
               <th scope="col" className="py-3 px-6  font-ptserif" />
             )}
-            {props.headerColumns.map(columnTitle => 
-              <th scope="col" className="py-3 px-6  font-ptserif">
-                {columnTitle}
+            {props.headerColumns.map((columnTitle, colIdx) => 
+              <th 
+                className="py-3 px-6  font-ptserif whitespace-nowrap cursor-pointer"
+                scope="col" 
+                onClick={() => toggleSort(colIdx)}
+              >
+                {columnTitle + (colIdx === sortOptions.column ? (sortOptions.order === 'ascending' ? ' ↓' : ' ↑') : '')}
               </th>
             )}
           </tr>
         </thead>
 
         <tbody>
-          {props.data.map((row, i) => (
-            <tr 
-              key={row[0]} 
-              className={`bg-white hover:bg-gray-50 cursor-pointer border-b ${
-                i === hoveredRowIndex ? 'relative' : ''
-              }`}
-              onMouseEnter={() => handleRowHover(i)}
-              onMouseLeave={() => handleRowHover(null)}
-            >
-              {/* Thumbnail */}
-              {props.thumbnails && (
-                <td className=" font-ptserif">
-                  <img width={50} height={50} src={props.thumbnails[i]} />
-                </td>
-              )}
+          {props.data
+            .sort((a, b) => compare(a[sortOptions.column], b[sortOptions.column], sortOptions.order))
+            .map((row, i) => (
+              <tr 
+                key={row[0]} 
+                className={`bg-white hover:bg-gray-50 cursor-pointer border-b ${
+                  i === hoveredRowIndex ? 'relative' : ''
+                }`}
+                onMouseEnter={() => handleRowHover(i)}
+                onMouseLeave={() => handleRowHover(null)}
+              >
+                {/* Thumbnail */}
+                {props.thumbnails && (
+                  <td className=" font-ptserif">
+                    <img width={50} height={50} src={props.thumbnails[i]} />
+                  </td>
+                )}
 
-              {/* ID */}
-              <th scope="row" className="py-4 px-6 font-medium text-black whitespace-nowrap  font-ptserif">
-                {row[0]}
-              </th>
+                {/* ID */}
+                <th scope="row" className="py-4 px-6 font-medium text-black whitespace-nowrap  font-ptserif">
+                  {row[0]}
+                </th>
 
-              {/* Data */}
-              {row.slice(1).map(item => (
-                <td className="py-4 px-6  font-ptserif">
-                  {item?.toString() || 'None'}
-                </td>
-              ))}
+                {/* Data */}
+                {row.slice(1).map(item => (
+                  <td className="py-4 px-6  font-ptserif">
+                    {item?.toString() || 'None'}
+                  </td>
+                ))}
 
-              {/* Edit Icon */}
-              {(i === hoveredRowIndex) && props.onEdit && (
-                <td className="absolute right-0 top-0 bottom-0 flex items-center justify-center">
-                  <button 
-                    onClick={() => {
-                      if (row[0] && props.onEdit)
-                        props.onEdit(row[0]);
-                    }}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-12 w-12 text-white-500 hover:text-white-600 cursor-pointer"
-                      fill="black"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                      >
-                      <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
-                    </svg>
-                  </button>
-                </td>
-              )}
-            </tr>
+                {/* Edit Icon */}
+                {(i === hoveredRowIndex) && props.onEdit && (
+                  <td className="absolute right-0 top-0 bottom-0 flex items-center justify-center">
+                    <button 
+                      onClick={() => {
+                        if (row[0] && props.onEdit)
+                          props.onEdit(row[0]);
+                      }}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-12 w-12 text-white-500 hover:text-white-600 cursor-pointer"
+                        fill="black"
+                        width={24}
+                        height={24}
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        >
+                        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/>
+                      </svg>
+                    </button>
+                  </td>
+                )}
+              </tr>
           ))}
         </tbody>
       </table>
