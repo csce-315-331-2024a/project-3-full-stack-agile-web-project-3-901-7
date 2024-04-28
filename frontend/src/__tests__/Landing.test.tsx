3b1cb64c7db749d93365c88e3eda3422e6eed3eb
WarrenWu4 <warrenweiwu04@gmail.com>
2024-04-28 18:53:16 -0500

    added some landing page tests

--- frontend/src/__tests__/Landing.test.tsx
+++ frontend/src/__tests__/Landing.test.tsx
@@ -1,9 +1,18 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Landing from "../pages/Landing"
 import { BrowserRouter } from "react-router-dom";
 
 describe("Landing Page Tests", () => {
-    it("should render without crashing", () => {
-        render(<Landing/>, { wrapper: BrowserRouter });
-    })
+        it("should render without crashing", () => {
+            render(<Landing/>, { wrapper: BrowserRouter });
+        })
+        it("navigates to correct route when buttons are clicked", () => {
+            render(<Landing/>, { wrapper: BrowserRouter });
+
+            fireEvent.click(screen.getByText("I am a customer"));
+            expect(window.location.pathname).toBe('/menu');
+
+            fireEvent.click(screen.getByText("I am a cashier"));
+            expect(window.location.pathname).toBe('/cashier/login');
+        })
 })
