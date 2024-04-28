9ffd80a4692ab4c1a232bb1081855de4d51a3d4f
WarrenWu4 <warrenweiwu04@gmail.com>
2024-04-28 18:33:44 -0500

    resetup frontend unit test with vitest instead of jest since babel can go suck it

--- frontend/src/__tests__/Landing.test.tsx
+++ frontend/src/__tests__/Landing.test.tsx
@@ -1,25 +1,9 @@
-import { render } from '@testing-library/react';
-import Landing from '../pages/Landing';
+import { render } from "@testing-library/react"
+import Landing from "../pages/Landing"
+import { BrowserRouter } from "react-router-dom";
 
-test('Testing Landing Page', () => {
-    // test that the landing page renders correctly
-    render(<Landing />);
-
-    // check that navbar is rendered within the Landing component
-
-    // test that clickong in each button triggers the correct navigation use useNavigate from react-router-dom
-
-    // verify that clicking on each button navigates the user to the correct route
-
-    // test that the buttons have the correct styling applied based on their state
-
-    // ensure that buttons are accessible to useers with disabilities by checking for appropriate ARIA attributes and keyboard navigation
-    
-    // check that the view readme link directs the user to the correct external URL and opens in a new tab
-
-    // verify that the text elements are displayed with correct fonts, sizes, and alignments
-
-    // test that the components displays correctly on different screen sizes and orientations
-
-    
-});
+describe("Landing Page Tests", () => {
+    it("should render without crashing", () => {
+        render(<Landing/>, { wrapper: BrowserRouter });
+    })
+})
