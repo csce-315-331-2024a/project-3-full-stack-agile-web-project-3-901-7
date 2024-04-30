e2c9d0ef8724646a87ccdac009eb0641bca734dc
shweta-kumaran <shweta.kumaran@gmail.com>
2024-04-30 12:52:20 -0500

    Fixed reload issue with translate.

--- frontend/src/components/GoogleTranslate.tsx
+++ frontend/src/components/GoogleTranslate.tsx
@@ -1,46 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 const GoogleTranslate: React.FC = () => {
+  const scriptLoaded = useRef<boolean>(false);
+  
   useEffect(() => {
     const id = 'google_translate_element';
     const scriptId = 'google-translate-script';
-    
-    if (!window.googleTranslateElementInit) {
-      window.googleTranslateElementInit = function() {
+
+    if (!scriptLoaded.current) {
+      const script = document.createElement('script');
+      script.id = scriptId;
+      script.src = "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+      script.async = true;
+
+      window.googleTranslateElementInit = function () {
         new window.google.translate.TranslateElement(
-          { pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
+          { pageLanguage: 'en',layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
           id
         );
       };
-    }
 
-    if (!document.getElementById(scriptId)) {
-      const script = document.createElement('script');
-      script.id = scriptId;
-      script.src = "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-      script.async = true;
       document.body.appendChild(script);
+      scriptLoaded.current = true;
     }
 
     return () => {
     };
-  }, []); 
+  }, []);
 
-  
+
   return (
     <div 
       id="google_translate_element" 
       style={{ 
         float: 'right',
         paddingLeft: '14px',
-        paddingRight: '14px', // Adjust the padding as needed for clickable area
+        paddingRight: '14px',
         lineHeight: '0',
-        // Other styles as needed
       }} 
     />
   );
 };
 
 export default GoogleTranslate;
-
 
