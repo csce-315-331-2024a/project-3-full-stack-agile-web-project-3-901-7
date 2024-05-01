914e1bfe4f3a4a00b05bac1ffcfc22e31b72ff03
WarrenWu4 <warrenweiwu04@gmail.com>
2024-04-30 20:24:41 -0500

    temp stash landing

--- frontend/src/__tests__/Landing.test.tsx
+++ frontend/src/__tests__/Landing.test.tsx
@@ -1,17 +1,78 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react"
 import Landing from "../pages/Landing"
 import { BrowserRouter } from "react-router-dom";
+import { vi } from "vitest";
+import { TextSizeProvider } from "../TextSizeContext";
+
+const mockFetch = vi.fn();
+global.fetch = mockFetch;
+
+const customRender = (ui:any, options?:any) => {
+    return render(
+        <BrowserRouter>
+            <TextSizeProvider>
+            {ui}
+            </TextSizeProvider>
+        </BrowserRouter>,
+        options
+    )
+}
 
 describe("Landing Page Tests", () => {
-        it("should render without crashing", () => {
-            render(<Landing/>, { wrapper: BrowserRouter });
+    beforeEach(() => {
+        mockFetch.mockResolvedValueOnce({
+            json: async () => [
+                {
+                    _id: 1,
+                    name: "Test Name 1",
+                    price: 10,
+                    category: "Test Category 1",
+                    ingredientInfo: "bacon,cheese",
+                    startDate: new Date(),
+                    endDate: null,
+                    picture: "https://clipart-library.com/images_k/transparent-cheeseburger/transparent-cheeseburger-12.png",
+                    itemDesc: "Test Description 1",
+                },
+                {
+                    _id: 2,
+                    name: "Test Name 2",
+                    price: 20,
+                    category: "Test Category 2",
+                    ingredientInfo: "bacon,cheese",
+                    startDate: new Date(),
+                    endDate: null,
+                    picture: "https://clipart-library.com/images_k/transparent-cheeseburger/transparent-cheeseburger-12.png",
+                    itemDesc: "Test Description 2",
+                },
+                {
+                    _id: 3,
+                    name: "Test Name 3",
+                    price: 30,
+                    category: "Test Category 3",
+                    ingredientInfo: "bacon,cheese",
+                    startDate: new Date(),
+                    endDate: null,
+                    picture: "https://clipart-library.com/images_k/transparent-cheeseburger/transparent-cheeseburger-12.png",
+                    itemDesc: "Test Description 3",
+                }
+            ]
         })
-        it("navigates to correct route when buttons are clicked", () => {
-            render(<Landing/>, { wrapper: BrowserRouter });
+    })
+    afterEach(() => {
+        vi.clearAllMocks();
+    })
 
-            waitFor(() => {
-                const customerButton = screen.getByText("Loading");
-                expect(customerButton).toBeTruthy();
-            })
-        })
+    it("should render without crashing", () => {
+        customRender(<Landing/>);
+    })
+    // it("should have initial loading screen", () => {
+    //     customRender(<Landing/>);
+    //     expect(screen.getByText("Loading...")).toBeTruthy();
+    // })
+    it("should render and navigate all buttons and items after loading", async () => {
+        customRender(<Landing/>);
+        await waitFor(() => {
+            expect(screen.getByText("Loading...")).toBeTruthy();
+        });
+    })
 })
