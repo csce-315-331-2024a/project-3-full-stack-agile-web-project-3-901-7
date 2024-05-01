7f7c0e578bb84b838b418dafef826ae1dce86450
Andrew Beketov <andrewbeketov03@tamu.edu>
2024-04-30 19:07:51 -0500

    fix google translate styling

--- frontend/src/components/GoogleTranslate.tsx
+++ frontend/src/components/GoogleTranslate.tsx
@@ -4,9 +4,9 @@ import React, { useEffect, useRef } from 'react';
  * Google translate component
  * @returns Google translate
  */
-const GoogleTranslate: React.FC = () => {
-  const scriptLoaded = useRef<boolean>(false);
-  
+const GoogleTranslate = () => {
+  const scriptLoaded = useRef(false);
+
   useEffect(() => {
     const id = 'google_translate_element';
     const scriptId = 'google-translate-script';
@@ -19,9 +19,32 @@ const GoogleTranslate: React.FC = () => {
 
       window.googleTranslateElementInit = function () {
         new window.google.translate.TranslateElement(
-          { pageLanguage: 'en',layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
+          { pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE },
           id
         );
+
+        const styleSheet = document.createElement("style");
+        styleSheet.type = "text/css";
+        styleSheet.innerText = `
+          #google_translate_element img,
+          .goog-te-gadget-icon {
+            display: none !important;
+          }
+          .goog-te-gadget-simple {
+            border: none !important;
+            background-color: white !important;
+            width: 100% !important;
+            height: 100% !important;
+          }
+          .goog-te-menu-value {
+            width: 100% !important;
+            height: 100% !important;
+            display: inline-block !important;
+            line-height: normal !important;
+            font-size: 1em !important;
+          }
+        `;
+        document.head.appendChild(styleSheet);
       };
 
       document.body.appendChild(script);
@@ -29,18 +52,20 @@ const GoogleTranslate: React.FC = () => {
     }
 
     return () => {
+      const existingScript = document.getElementById(scriptId);
+      if (existingScript) {
+        existingScript.remove();
+      }
     };
   }, []);
 
-
   return (
     <div 
-      id="google_translate_element" 
-      style={{ 
-        float: 'right',
-        paddingLeft: '14px',
-        paddingRight: '14px',
-        lineHeight: '0',
+      id="google_translate_element"
+      style={{
+        width: '100%',
+        height: '100%',
+        display: 'block'
       }} 
     />
   );
