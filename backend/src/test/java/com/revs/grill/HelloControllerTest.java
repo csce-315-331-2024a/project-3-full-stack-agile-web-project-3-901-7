af46f6f2d56cb8f32181206b16d9e39751f44bf2
Beketovian <andrewbeketov03@gmail.com>
2024-05-03 07:34:07 -0500

    constructive and destructive tests

--- backend/src/test/java/com/revs/grill/HelloControllerTest.java
+++ backend/src/test/java/com/revs/grill/HelloControllerTest.java
@@ -29,22 +29,25 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
 
-	@Autowired
-	private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	public void getHello() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("wassup bro :)")));
-	}
-
-	@Test
+    @Test
     public void getAllUsersTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/user/findAll")
                 .accept(MediaType.APPLICATION_JSON))
@@ -52,7 +55,7 @@ public class HelloControllerTest {
                 .andExpect(jsonPath("$", hasSize(greaterThan(0))));
     }
 
-	@Test
+    @Test
     public void getUsersByIdTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/user/findById")
                 .param("userIds", "1", "2")
@@ -89,6 +92,71 @@ public class HelloControllerTest {
     }
 
     @Test
+    public void getAllOrdersTest() throws Exception {
+        int limit = 10;
+        mvc.perform(MockMvcRequestBuilders.get("/order/findAll")
+                .param("limit", String.valueOf(limit)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(greaterThan(0))));
+    }
+
+    @Test
+    public void getOrdersByDateRangeTest() throws Exception {
+        String start = "2024-5-01";
+        String end = "2024-5-03";
+        mvc.perform(MockMvcRequestBuilders.get("/order/findInDateRange")
+                .param("start", start)
+                .param("end", end))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(greaterThan(0))));
+    }
+
+    @Test
+    public void getItemsByIdTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/item/findById")
+                .param("itemIds", "1", "2")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+    // broken
+    // @Test
+    // public void getOrdersByStatusTest() throws Exception {
+    //     String status = "completed";
+    //     mvc.perform(MockMvcRequestBuilders.get("/order/findByStatus")
+    //             .param("status", status))
+    //             .andExpect(status().isOk())
+    //             .andExpect(jsonPath("$", hasSize(greaterThan(0))));
+    // }
+
+    @Test
+    public void getOneOrderByIdTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/order/findOneById")
+                .param("orderId", "1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._id", equalTo(1)));
+    }
+
+    @Test
+    public void getIngredientsByIdTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/ingredient/findById")
+                .param("ingredientIds", "1", "2")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    public void getOneIngredientByIdTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/ingredient/findOneById")
+                .param("ingredientId", "1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$._id", equalTo(1)));
+    }
+
+    @Test
     public void getAllRolesTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/role/findAll"))
                 .andExpect(status().isOk())
@@ -112,11 +180,12 @@ public class HelloControllerTest {
                 .andExpect(jsonPath("$.success", equalTo(true)));
     }
 
+    // naw
     // @Test
     // public void editRoleTest() throws Exception {
     //     mvc.perform(MockMvcRequestBuilders.post("/role/edit")
     //             .contentType(MediaType.APPLICATION_JSON)
-    //             .content("{\"name\":\"Updated Role\"}"))
+    //             .content("{\"name\":\"Test Role\"}"))
     //             .andExpect(status().isOk())
     //             .andExpect(jsonPath("$.success", equalTo(true)));
     // }
@@ -125,7 +194,7 @@ public class HelloControllerTest {
     public void deleteRoleTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.post("/role/delete")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"Role to delete\"}"))
+                .content("{\"name\":\"Test Role\"}"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success", equalTo(true)));
     }
@@ -153,48 +222,152 @@ public class HelloControllerTest {
     //             .andExpect(jsonPath("$", hasSize(2)));
     // }
 
-    // @Test
-    // public void getOneItemByIdTest() throws Exception {
-    //     mvc.perform(MockMvcRequestBuilders.get("/item/findOneById")
-    //             .param("itemId", "1")
-    //             .accept(MediaType.APPLICATION_JSON))
-    //             .andExpect(status().isOk())
-    //             .andExpect(jsonPath("$.id", equalTo(1)));
-    // }
+    @Test
+    public void getOneItemByIdTest() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/item/findOneById")
+                .param("itemId", "1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", equalTo(1)));
+    }
 
     @Test
     public void getItemsByCategoryTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/item/findByCategory")
-                .param("category", "Test Category")
+                .param("category", "Burger")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(greaterThan(0))));
     }
 
+    // insert item should be ID 25
     @Test
     public void insertItemTest() throws Exception {
+        String itemJson = "{\"name\":\"New Item\",\"price\":9.99,\"category\":\"Beverage\",\"ingredients\":[{\"_id\":1,\"name\":\"Sugar\"}],\"startDate\":\"2024-05-01\",\"endDate\":\"2024-05-31\",\"picture\":\"url_to_picture\",\"itemDesc\":\"A new beverage item.\"}";
         mvc.perform(MockMvcRequestBuilders.post("/item/insert")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"Test Item\",\"category\":\"Test Category\"}"))
+                .content(itemJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", equalTo(true)));
+    }
+
+    // edit this item of ID 25
+    @Test
+    public void editItemTest() throws Exception {
+        String itemJson = "{\"_id\":25,\"name\":\"Updated Item\",\"price\":10.99,\"category\":\"Snack\",\"ingredients\":[{\"_id\":2,\"name\":\"Salt\"}],\"startDate\":\"2024-06-01\",\"endDate\":\"2024-06-30\",\"picture\":\"new_url_to_picture\",\"itemDesc\":\"Updated description of the item.\"}";
+        mvc.perform(MockMvcRequestBuilders.post("/item/edit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(itemJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", equalTo(true)));
+    }
+
+    // delete the new item of ID 25
+    @Test
+    public void deleteItemTest() throws Exception {
+        int itemId = 25;
+        mvc.perform(MockMvcRequestBuilders.post("/item/deleteById")
+                .param("itemId", String.valueOf(itemId)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success", equalTo(true)));
+    }
+
+    // this ingredient should be created as ID 45
+    @Test
+    public void insertIngredientTest() throws Exception {
+        String ingredientJson = "{\"name\":\"New Ingredient\",\"quantity\":100,\"minQuantity\":50,\"unitPrice\":5.99,\"supplier\":\"Supplier X\"}";
+        mvc.perform(MockMvcRequestBuilders.post("/ingredient/insert")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ingredientJson))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void insertIngredientTestQuantityZero() throws Exception {
+        String ingredientJson = "{\"name\":\"New Ingredient\",\"quantity\":,\"minQuantity\":50,\"unitPrice\":5.99,\"supplier\":\"Supplier X\"}";
+        mvc.perform(MockMvcRequestBuilders.post("/ingredient/insert")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ingredientJson))
+                .andExpect(status().isBadRequest());
+    }
+
+    // edit this ingredient of ID 45
+    @Test
+    public void editIngredientTest() throws Exception {
+        String ingredientJson = "{\"_id\":45,\"name\":\"Updated Ingredient\",\"quantity\":150,\"minQuantity\":75,\"unitPrice\":6.99,\"supplier\":\"Supplier Y\"}";
+        mvc.perform(MockMvcRequestBuilders.post("/ingredient/edit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(ingredientJson))
+                .andExpect(status().isOk());
+    }
+
+
+    // @Test
+    // public void editIngredientTestNameEmpty() throws Exception {
+    //     String ingredientJson = "{\"_id\":60,\"name\":\"\",\"quantity\":150,\"minQuantity\":75,\"unitPrice\":6.99,\"supplier\":\"Supplier Y\"}";
+    //     mvc.perform(MockMvcRequestBuilders.post("/ingredient/edit")
+    //             .contentType(MediaType.APPLICATION_JSON)
+    //             .content(ingredientJson))
+    //             .andExpect(status().isOk());
+    // }
+
+    // @Test
+    // public void editIngredientTestminQuantEmpty() throws Exception {
+    //     String ingredientJson = "{\"_id\":45,\"name\":\"Updated Ingredient\",\"quantity\":150,\"minQuantity\":,\"unitPrice\":6.99,\"supplier\":\"Supplier Y\"}";
+    //     mvc.perform(MockMvcRequestBuilders.post("/ingredient/edit")
+    //             .contentType(MediaType.APPLICATION_JSON)
+    //             .content(ingredientJson))
+    //             .andExpect(status().isOk());
+    // }
+
+    // delete the new ingredient of ID 45
+    @Test
+    public void deleteIngredientTest() throws Exception {
+        int ingredientId = 45;
+        mvc.perform(MockMvcRequestBuilders.post("/ingredient/deleteById")
+                .param("ingredientId", String.valueOf(ingredientId)))
+                .andExpect(status().isOk());
+    }
+
+    // stack overflow
+    // @Test
+    // public void getSellsTogetherTest() throws Exception {
+    //     String startDate = "2024-04-25";
+    //     String endDate = "2024-05-01";
+    //     mvc.perform(MockMvcRequestBuilders.get("/item/sellsTogether")
+    //             .param("start", startDate)
+    //             .param("end", endDate))
+    //             .andExpect(status().isOk());
+    // }
+
+
+
+    @Test
+    public void insertOrderTest() throws Exception {
+        String orderJson = "{\"numItems\":3,\"total\":120.50,\"orderInfo\":\"New online order\",\"dateTime\":\"2024-04-21\",\"status\":\"received\"}";
+        mvc.perform(MockMvcRequestBuilders.post("/order/insert")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(orderJson))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success", equalTo(true)));
     }
 
     @Test
-    public void editItemTest() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.post("/item/edit")
+    public void insertOrderTestNoQuantity() throws Exception {
+        String orderJson = "{\"numItems\":3,\"total\":120.50,\"orderInfo\":\"New online order\",\"dateTime\":\"2024-04-21\",\"status\":\"received\",\"itemToQuantity\":}";
+        mvc.perform(MockMvcRequestBuilders.post("/order/insert")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"Updated Item\",\"category\":\"Updated Category\"}"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.success", equalTo(true)));
+                .content(orderJson))
+                .andExpect(status().isBadRequest());
     }
 
     // @Test
-    // public void deleteItemTest() throws Exception {
-    //     mvc.perform(MockMvcRequestBuilders.post("/item/deleteById")
-    //             .param("itemId", "1"))
-    //             .andExpect(status().isOk())
-    //             .andExpect(jsonPath("$.success", equalTo(true)));
+    // public void editOrderTestBadReq() throws Exception {
+    //     String orderJson = "{\"_id\":25,\"numItems\":1,\"total\":200.00,\"orderInfo\":\"Updated order details\",\"dateTime\":\"2024-05-05\",\"status\":\"Completed\"}";
+    //     mvc.perform(MockMvcRequestBuilders.post("/order/edit")
+    //             .contentType(MediaType.APPLICATION_JSON)
+    //             .content(orderJson))
+    //             .andExpect(status().isOk());
     // }
 
     // @Test
@@ -206,7 +379,7 @@ public class HelloControllerTest {
     //             .andExpect(jsonPath("$", hasSize(greaterThan(0))));
     // }
 
-    // this shit bad dont insert empty order
+    // this bad dont insert empty order
     // @Test
     // public void insertOrderTest() throws Exception {
     //     mvc.perform(MockMvcRequestBuilders.post("/order/insert")
@@ -224,13 +397,6 @@ public class HelloControllerTest {
     //             .andExpect(jsonPath("$.success", equalTo(true)));
     // }
 
-    // @Test
-    // public void deleteOrderTest() throws Exception {
-    //     mvc.perform(MockMvcRequestBuilders.post("/order/deleteById")
-    //             .param("order", "{\"name\":\"Order to delete\"}"))
-    //             .andExpect(status().isOk())
-    //             .andExpect(jsonPath("$.success", equalTo(true)));
-    // }
 
     // @Test
     // public void getAllOrdersTest() throws Exception {
@@ -304,29 +470,29 @@ public class HelloControllerTest {
     @Test
     public void getIngredientsByNameTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/ingredient/findByName")
-                .param("ingredientName", "Test Ingredient")
+                .param("ingredientName", "bacon")
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(greaterThan(0))));
     }
 
-    @Test
-    public void insertIngredientTest() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.post("/ingredient/insert")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"Test Ingredient\"}"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", greaterThan(0)));
-    }
+    // @Test
+    // public void insertIngredientTest() throws Exception {
+    //     mvc.perform(MockMvcRequestBuilders.post("/ingredient/insert")
+    //             .contentType(MediaType.APPLICATION_JSON)
+    //             .content("{\"name\":\"Test Ingredient\"}"))
+    //             .andExpect(status().isOk())
+    //             .andExpect(jsonPath("$", greaterThan(0)));
+    // }
 
-    @Test
-    public void editIngredientTest() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.post("/ingredient/edit")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content("{\"name\":\"Updated Ingredient\"}"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$", equalTo(true)));
-    }
+    // @Test
+    // public void editIngredientTest() throws Exception {
+    //     mvc.perform(MockMvcRequestBuilders.post("/ingredient/edit")
+    //             .contentType(MediaType.APPLICATION_JSON)
+    //             .content("{\"name\":\"Updated Ingredient\"}"))
+    //             .andExpect(status().isOk())
+    //             .andExpect(jsonPath("$", equalTo(true)));
+    // }
 
     // @Test
     // public void deleteIngredientTest() throws Exception {
